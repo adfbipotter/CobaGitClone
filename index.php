1c4a55aad31e9dc5c1a4b8571023c3d10290ea88
Adam MB <adfbipotter@windowslive.com>
2015-09-20 21:57:00 +0700

    Menambahkan index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+	echo "Selamat Datang";
+	echo"<br>";
+?>
+
+
+Saeful Nur Kamalludin
+kholili rachman
+Muhammad Arif Fathurohman
+Bona Brian Siagian
+Rhozi Barrur
+Hansen Feriandy
+Muh. Kanda Wibawa Putra
+Rangga Djatikusuma Lukman
+Yoga T. Nugroho
+Valerian Dwi Purnomo
+ganjar hadiatna
+Muhammad Shyhab Albi
+iqbal naufal firdaus
+Ramdhan Rizki
+Hansen Feriandy
+Ginanjar Angga
+kasifil aziz
+Dadi Rosadi
+Jessica Liem
+irsyad fauzan
+rizal fadilah
+Senna Prasetya
+destu wijayanto
+Derandi Hermanda
+Muhammad Fahmi Irfan
+Nursin
+Rifky Vianda
+Randy Vianda Putra
+Daniyal Ahmad Rizaldhi Onil
+Muhamad Nur Arifin
+Rizky Risman
+Reza Marten
+Joko Purwanto
+Rizky Gunardi
+Rio Ardhityo
